7d9d083b432a3ec070616aa4dcf15d53ac2f5e15
Meianna Oeser <meianna.oeser@gmail.com>
2020-05-31 13:53:08 -0700

    started base functionality

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const questions = [
+var inquirer = require("inquirer");
+var axios = require("axios");
+var fs = require("fs");
 
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "Enter Github username",
+  },
 ];
 
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 function init() {
-
+  inquirer.prompt(questions).then(function (response) {
+    console.log(response);
+    axios
+      .get(`https://api.github.com/users/${response.github}`)
+      .then(function (response) {
+        console.log(response);
+        var readMe = `
+#username: ${response.data.login}
+[link to github profile!](${response.data.url})
+        `;
+        return readMe;
+      })
+      .then(function (data) {
+        fs.writeFileSync("./README.md", data, function (error) {
+          if (error) throw error;
+          console.log("ReadMe generated!");
+        });
+      });
+  });
 }
 
 init();
