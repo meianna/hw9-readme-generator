227f0e5b4e68cdf4bcdfd108841d3d778852da07
Meianna Oeser <meianna.oeser@gmail.com>
2020-06-15 09:04:42 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 
-#username: meianna
-[link to github profile!](https://api.github.com/users/meianna)
+        # Username: meianna
+        [link to github profile!](https://github.com/meianna)
+        /n# Email: meianna.oeser@gmail.com
+        /n![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+        /nProject | Details
+        /n------- | -------
+        /nProject Title | asdf
+        /nDescription | sdf
+        /nInstallation | sdf 
+        /nTesting | daf
+        /nContributing | adf
+        /nLicense | MIT
         
--- index.js
+++ index.js
@@ -1,12 +1,57 @@
-var inquirer = require("inquirer");
-var axios = require("axios");
-var fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+const util = require("util");
+const { userInfo } = require("os");
+let userInput = {};
 
 const questions = [
   {
     type: "input",
-    name: "github",
+    name: "username",
     message: "Enter Github username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please describe your project",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "Please provide installation instructons",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please explain how others can use your code",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Who helped contribute to this code?",
+  },
+  {
+    type: "list",
+    name: "license",
+    choices: ["MIT", "ISC", "APACHE 2.0"],
+    message: "Please choose a license",
+  },
+  {
+    type: "input",
+    name: "test",
+    choices: ["manual", "none", "chai", "mocha", "jest"],
+    message: "How would you like to test the app?",
   },
 ];
 
@@ -15,13 +60,24 @@ function writeToFile(fileName, data) {}
 function init() {
   inquirer.prompt(questions).then(function (response) {
     console.log(response);
-    axios
-      .get(`https://api.github.com/users/${response.github}`)
-      .then(function (response) {
-        console.log(response);
-        var readMe = `
-# Username: ${response.data.login}
-[link to github profile!](${response.data.html_url})
+    userInput = response;
+    return axios
+      .get(`https://api.github.com/users/${response.username}`)
+      .then(function (axiosResponse) {
+        console.log(axiosResponse.data);
+        let readMe = `
+        # Username: ${axiosResponse.data.login}
+        [link to github profile!](${axiosResponse.data.html_url})
+        /n# Email: ${userInput.email}
+        /n![GitHub license](https://img.shields.io/badge/license-${userInput.license}-blue.svg)
+        /nProject | Details
+        /n------- | -------
+        /nProject Title | ${userInput.title}
+        /nDescription | ${userInput.description}
+        /nInstallation | ${userInput.install} 
+        /nTesting | ${userInput.test}
+        /nContributing | ${userInput.contributing}
+        /nLicense | ${userInput.license}
         `;
         return readMe;
       })
